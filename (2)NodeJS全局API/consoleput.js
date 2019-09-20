9ac45c6a27ebf66ec675219da103b0c36495f73d
冀琳 <33645017+Jilinlin@users.noreply.github.com>
2019-09-20 14:31:55 +0800

    Add files via upload

---
+++ (2)NodeJS全局API/consoleput.js
@@ -0,0 +1,4 @@
+console.log(process.platform);//当前Node.js进程运行所在的操作系统平台
+console.log(process.pid);//当前Node.js进程的PID
+console.log(process.argv[0]);//当前Node.js执行程序的文件路径
+console.log(process.argv[1]);//当前
